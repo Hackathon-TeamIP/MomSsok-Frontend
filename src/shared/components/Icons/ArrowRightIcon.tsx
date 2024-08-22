f98a649ea645bacdd18c746273c3632c04b9e8fb
yejine2 <yejine2@naver.com>
2024-08-22 18:21:17 +0900

    fix: svg 아이콘 property 에러 수정

--- src/shared/components/Icons/ArrowRightIcon.tsx
+++ src/shared/components/Icons/ArrowRightIcon.tsx
@@ -14,8 +14,8 @@ export const ArrowRightIcon = React.forwardRef<SVGSVGElement, IconProps>(
         ref={forwardedRef}
       >
         <path
-          fill-rule="evenodd"
-          clip-rule="evenodd"
+          fillRule="evenodd"
+          clipRule="evenodd"
           d="M0.0666809 1.87054L1.45977 0.5L8.06668 7L1.45977 13.5L0.0666809 12.1295L5.28051 7L0.0666809 1.87054Z"
           fill={color}
         />
