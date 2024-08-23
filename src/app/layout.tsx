77110f873f10ed9687242426393656bf804b554b
yejine2 <yejine2@naver.com>
2024-08-23 14:41:32 +0900

    style: layout 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import "@/shared/styles";
-import { NextLayout } from "@/shared/components/Layout/Layout";
 import { pretendard } from "@/shared/styles/fonts";
 import type { Metadata } from "next";
 
-export const metadata: Metadata = {};
+export const metadata: Metadata = {
+  title: "맘쏙",
+  description: "내 주변 베리어프리 놀이터",
+};
 
 export default function RootLayout({
   children,
@@ -13,7 +15,8 @@ export default function RootLayout({
   return (
     <html lang="ko" className={`${pretendard.variable}`}>
       <body>
-        <NextLayout>{children}</NextLayout>
+        {/* <NextLayout>{children}</NextLayout> */}
+        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import Header from "@/features/main/components/Header";
 import { SpacesList } from "@/features/main/components/SpacesList";
+import TabBar from "@/shared/components/Navigation/TabBar";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main>
+    <main className="pb-[83px]">
       <Header />
       <Suspense fallback={<></>}>
         <SpacesList />
       </Suspense>
+      <TabBar />
     </main>
   );
 }
