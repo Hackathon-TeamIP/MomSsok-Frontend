e196494d7df7950ae5f6c61c9df2f4aae9fb3eb5
Dawon00 <dawon000203@gmail.com>
2024-08-25 17:59:35 +0900

    feat: 검색 결과 페이지 ui 구현

--- src/features/search/components/SearchResults.tsx
+++ src/features/search/components/SearchResults.tsx
@@ -1,51 +1,51 @@
+import { Divider } from "@/shared/components/Divider";
+import { search_results } from "../api/data";
+import AccessibilityFilter from "./AccessibilityFilter";
+
 interface SearchResultsProps {
   query: string;
 }
 
 export default function SearchResults({ query }: SearchResultsProps) {
-  const results = [
-    {
-      id: 1,
-      name: "공간 이름",
-      description: "아이와아이들이 좋아하는 공간에 대한 설명",
-    },
-    {
-      id: 2,
-      name: "공간 이름",
-      description: "아이와아이들이 좋아하는 공간에 대한 설명",
-    },
-    {
-      id: 3,
-      name: "공간 이름",
-      description: "아이와아이들이 좋아하는 공간에 대한 설명",
-    },
-    {
-      id: 4,
-      name: "공간 이름",
-      description: "아이와아이들이 좋아하는 공간에 대한 설명",
-    },
-    {
-      id: 5,
-      name: "공간 이름",
-      description: "아이와아이들이 좋아하는 공간에 대한 설명",
-    },
-  ];
+  const results = search_results;
 
   return (
     <div>
-      <h2 className="text-xl font-bold mb-4">
-        &apos;{query}&apos;에 대한 검색 결과
-      </h2>
+      <AccessibilityFilter />
+      <div className="text-sm mt-5">전체 {results.data.length}개</div>
 
       <ul>
-        {results.map((result) => (
-          <li key={result.id} className="mb-4 p-4 border rounded">
-            <h3 className="font-semibold">{result.name}</h3>
-            <p>{result.description}</p>
-            <div className="mt-2 text-sm text-gray-500">
-              <span className="mr-4">⭐ 4.7</span>
-              <span>리뷰 112</span>
+        {results.data.map((result, index) => (
+          <li key={result.id} className="flex flex-col">
+            <div className="flex items-center">
+              <div className="bg-gray-100 min-w-[100px] h-[100px] rounded-lg" />
+
+              <div className="p-4 w-full">
+                <h3 className="font-semibold text-lg mb-1">{result.name}</h3>
+                <p className="text-gray-400 text-sm mb-1">
+                  {result.description}
+                </p>
+
+                <div className="flex text-xs text-gray-400">
+                  <div>{result.location}</div>
+                  <div>・</div>
+                  <div>{result.type}</div>
+                  <div>・</div>
+                  <div>
+                    <span className="text-red-300">{result.holiday}요일</span>
+                    &nbsp;정기휴무
+                  </div>
+                </div>
+
+                <div className="mt-2 text-xs text-gray-500">
+                  <span className="mr-4">⭐ 4.7</span>
+                  <span>리뷰 112</span>
+                </div>
+              </div>
             </div>
+            {index < results.data.length - 1 && (
+              <Divider className="mb-2 mt-2" />
+            )}
           </li>
         ))}
       </ul>
