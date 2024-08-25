5bf9e7ee610de75cea5d032d42f57f9e80f2fa9e
Dawon00 <dawon000203@gmail.com>
2024-08-25 16:39:20 +0900

    refactor: 예시 데이터 분리

--- src/features/search/components/RecommendedSpaces.tsx
+++ src/features/search/components/RecommendedSpaces.tsx
@@ -1,35 +1,10 @@
 import { Divider } from "@/shared/components/Divider";
 import { CloseIcon } from "@/shared/components/Icons";
 import ArrowRightIcon from "@/shared/components/Icons/ArrowRightIcon";
+import recommended_spaces from "../api/data";
 
 export default function RecommendedSpaces() {
-  const spaces = [
-    {
-      id: 1,
-      name: "서초구 작은음악회 집",
-      description: "월드컵 경기장의 작고 귀여운 아기자기한 키즈카페",
-    },
-    {
-      id: 2,
-      name: "서초구 작은음악회 집",
-      description: "월드컵 경기장의 작고 귀여운 아기자기한 키즈카페",
-    },
-    {
-      id: 3,
-      name: "서초구 작은음악회 집",
-      description: "월드컵 경기장의 작고 귀여운 아기자기한 키즈카페",
-    },
-    {
-      id: 4,
-      name: "서초구 작은음악회 집",
-      description: "월드컵 경기장의 작고 귀여운 아기자기한 키즈카페",
-    },
-    {
-      id: 5,
-      name: "서초구 작은음악회 집",
-      description: "월드컵 경기장의 작고 귀여운 아기자기한 키즈카페",
-    },
-  ];
+  const spaces = recommended_spaces;
 
   return (
     <div>
