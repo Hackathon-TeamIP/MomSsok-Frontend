845bda0aed85ef4152a0e58174f54aa6741aba5b
yejine2 <yejine2@naver.com>
2024-08-27 21:12:31 +0900

    feat: 공간 목록 무한 스크롤 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Header from "@/features/main/components/Header";
+import { SpacesListSkeleton } from "@/features/main/components/Skeleton";
 import { SpacesList } from "@/features/main/components/SpacesList";
 import { TabBar } from "@/shared/components/Navigation/TabBar";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="pb-[83px]">
+    <main className="pb-[60px]">
       <Header />
-      <Suspense fallback={<></>}>
+      <Suspense fallback={<SpacesListSkeleton />}>
         <SpacesList />
       </Suspense>
       <TabBar />
