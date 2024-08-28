86c77939debde27ef2a847be749b7db7353b444e
yejine2 <yejine2@naver.com>
2024-08-28 10:30:43 +0900

    style: 스타일 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,22 +3,20 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <main className="grid min-h-full place-items-center bg-white px-6 pt-52">
-      <div className="text-center">
-        <p className="text-2xl font-semibold text-primary">404</p>
-        <h1 className="mt-4 text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
-          앗! 페이지가 없어요.
-        </h1>
-        <p className="mt-3 leading-7 text-gray-500">
-          요청하신 페이지가 존재하지 않습니다.
-        </p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Link
-            href="/"
-            className="rounded-md bg-primary px-4 py-2.5 text-sm font-semibold text-white shadow-sm "
-          >
-            메인으로 가기
-          </Link>
-        </div>
+      <p className="text-2xl font-semibold text-primary">404</p>
+      <h1 className="mt-4 text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl">
+        앗! 페이지가 없어요.
+      </h1>
+      <p className="mt-3 leading-7 text-gray-500">
+        요청하신 페이지가 존재하지 않습니다.
+      </p>
+      <div className="mt-10 flex items-center justify-center gap-x-6">
+        <Link
+          href="/"
+          className="rounded-md bg-primary px-4 py-2.5 text-sm font-semibold text-white shadow-sm "
+        >
+          메인으로 가기
+        </Link>
       </div>
     </main>
   );
--- src/features/main/components/Skeleton.tsx
+++ src/features/main/components/Skeleton.tsx
@@ -1,12 +1,15 @@
 export const SpacesListSkeleton = () => {
   return (
-    <div className="grid grid-cols-2 gap-x-3 gap-y-[23px] px-4 mt-[73px]">
-      {[...Array(10)].map((e, i) => (
-        <div
-          className="w-full aspect-square rounded-[10px] animate-pulse bg-gray-200 mt-2"
-          key={i}
-        />
-      ))}
+    <div className="px-4">
+      <div className="w-full h-[35px] animate-pulse bg-gray-200 mb-4 mt-6 rounded-md" />
+      <div className="grid grid-cols-2 gap-x-3 gap-y-[23px]">
+        {[...Array(10)].map((e, i) => (
+          <div
+            className="w-full aspect-square rounded-[10px] animate-pulse bg-gray-200 mt-2"
+            key={i}
+          />
+        ))}
+      </div>
     </div>
   );
 };
